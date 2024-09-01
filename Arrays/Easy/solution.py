1055fb472150c63d26418df3c15739906c9a7d78
brilliantbadger <chae.samuel13alt@gmail.com>
2024-09-01 15:28:49 +0000

    created templates for easy medium hard questions; solved array easy

---
+++ Arrays/Easy/solution.py
@@ -0,0 +1,14 @@
+for _ in range(int(input())):
+    database = []
+    report = []
+
+    inp = input().split()
+    for i in range(int(inp[0])):
+        database.append(input())
+    for i in range(int(inp[1])):
+        report.append(input())
+    
+    inspect = list(set(database) - set(report))
+    
+    for i in inspect:
+        print(i)
