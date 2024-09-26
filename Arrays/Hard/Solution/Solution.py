71cccdcba9335aa998b18e331f24dff10ee1ad91
brilliantbadger <chae.samuel13alt@gmail.com>
2024-09-26 11:14:02 +0000

    little optimization

--- Arrays/Hard/Solution/Solution.py
+++ Arrays/Hard/Solution/Solution.py
@@ -27,27 +27,28 @@ for _ in range(int(input())):
 
     for rowIndex, row in enumerate(layout[:-1]):
         for colIndex in range(1, len(row), 2):
+            if(layout[rowIndex][colIndex] == '_'):
+                disToRightEdge = (len(row) - colIndex)/2
+                disToBot = len(layout) - rowIndex - 1
 
-            disToRightEdge = (len(row) - colIndex)/2
-            disToBot = len(layout) - rowIndex - 1
-            if(disToBot > disToRightEdge):
-                maxSquareSize = int(disToRightEdge)
-            else:
-                maxSquareSize = int(disToBot)
+                if(disToBot > disToRightEdge):
+                    maxSquareSize = int(disToRightEdge)
+                else:
+                    maxSquareSize = int(disToBot)
 
-            for squareSize in range(1, maxSquareSize+1):
-                isSquare = True
-                for j in range(0, squareSize*2, 2):
-                    if(layout[rowIndex][colIndex+j] != '_'):
-                        isSquare = False
-                    if(layout[rowIndex+squareSize][colIndex+j] != '_'):
-                        isSquare = False   
-                for j in range(1,squareSize+1):
-                    if(layout[rowIndex+j][colIndex-1] != '|'):
-                        isSquare = False
-                    if(layout[rowIndex+j][colIndex+(squareSize*2)-1] != '|'):
-                        isSquare = False
-                if(isSquare):
-                    numOfSquares += 1
+                for squareSize in range(1, maxSquareSize+1):
+                    isSquare = True
+                    for j in range(0, squareSize*2, 2):
+                        if(layout[rowIndex][colIndex+j] != '_'):
+                            isSquare = False
+                        if(layout[rowIndex+squareSize][colIndex+j] != '_'):
+                            isSquare = False   
+                    for j in range(1,squareSize+1):
+                        if(layout[rowIndex+j][colIndex-1] != '|'):
+                            isSquare = False
+                        if(layout[rowIndex+j][colIndex+(squareSize*2)-1] != '|'):
+                            isSquare = False
+                    if(isSquare):
+                        numOfSquares += 1
     
     print(numOfSquares)
